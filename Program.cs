7d55d292a90d39a2683dd4a885fe4e6b5e344b1c
Tomi <tomitoye2016@gmail.com>
2021-08-26 17:03:20 -0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Division
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Program for subtraction of two numbers
+            Console.WriteLine("===== PROGRAM FOR DIVISION OF TWO NUMBERS =====");
+
+            // Declare variables
+            float firstNumber;
+            float secondNumber;
+            float result;
+
+            // Prompt user to enter the first number
+            Console.Write("Please enter the first number: ");
+            firstNumber = int.Parse(Console.ReadLine());
+
+            // Prompt user to enter the second number
+            Console.Write("Please enter the second number: ");
+            secondNumber = int.Parse(Console.ReadLine());
+
+            // Perform subtraction
+            result = firstNumber / secondNumber;
+
+            // Display result
+            Console.WriteLine("The result of " + firstNumber + " / " + secondNumber + " is: " + result);
+        }
+    }
+}
